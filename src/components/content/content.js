c6dd87e68c9834ecd6f44e64d5524707b25489a2
OlegPichkur <oleg1pichkur@gmail.com>
2024-07-29 20:27:30 +0300

    05 refactored content, tours

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -1,37 +1,21 @@
 /* eslint-disable */
 import React, { Component } from "react";
-import ToursService from "../../services/servicesTours";
+
 import Tours from "../tours/tours";
 
 class Content extends Component {
-  state = {
-    tours: [],
-  };
-
-  toursService = new ToursService();
+  state = {};
 
   getParams = () => {};
 
-  getTours = () => {
-    this.toursService.getAllTours().then((res) =>
-      this.setState({
-        tours: res.data.data,
-      })
-    );
-  };
-
   componentDidMount() {
-    this.getTours();
     this.getParams();
   }
 
   render() {
-    const { tours } = this.state;
     return (
       <main className="main">
-        <div className="card-container">
-          <Tours tours={tours} />
-        </div>
+        <Tours />
       </main>
     );
   }
--- src/components/tours/tours.js
+++ src/components/tours/tours.js
@@ -1,82 +1,33 @@
 /* eslint-disable */
+import React, { Component } from "react";
+import ToursService from "../../services/servicesTours";
+import Tour from "../tour/tour";
 
-const Tours = ({ tours }) => {
-  const elements = tours.map((item) => {
-    return (
-      <div className="card" key={item._id}>
-        <div className="card__header">
-          <div className="card__picture">
-            <div className="card__picture-overlay">
-              &nbsp;
-              <img
-                className="card__picture-img"
-                src={`/img/tours/${item.imageCover}`}
-                alt={item.name}
-              />
-            </div>
-          </div>
-          <h3 className="heading-tertirary">
-            <span>{item.name} </span>
-          </h3>
-        </div>
-        <div className="card__details">
-          <h4 className="card__sub-heading">
-            {item.difficulty} {item.duration}-day tour
-          </h4>
-          <p className="card__text">{item.summary}</p>
-          <div className="card__data">
-            <svg className="card__icon" viewBox="0 0 32 32">
-              <use xlinkHref="/img/icons.svg#icon-map-pin" />
-            </svg>
-            <span>{item.startLocation.description}</span>
-          </div>
-          <div className="card__data">
-            <svg className="card__icon" viewBox="0 0 32 32">
-              <use xlinkHref="/img/icons.svg#icon-calendar" />
-            </svg>
-            <span>
-              {new Date(item.startDates[0]).toLocaleString("en-US", {
-                month: "long",
-                year: "numeric",
-              })}
-            </span>
-          </div>
-          <div className="card__data">
-            <svg className="card__icon" viewBox="0 0 32 32">
-              <use xlinkHref="/img/icons.svg#icon-user" />
-            </svg>
-            <span>{item.maxGroupSize} people</span>
-          </div>
-          <div className="card__data">
-            <svg className="card__icon" viewBox="0 0 32 32">
-              <use xlinkHref="/img/icons.svg#icon-flag" />
-            </svg>
-            <span>{item.locations.length} stops</span>
-          </div>
-        </div>
-        <div className="card__footer">
-          <p>
-            <span className="card__footer-value"> {item.price} </span>
-            <span>|</span>
-            <span className="card__footer-text"> per person </span>
-          </p>
-          <p className="card__ratings">
-            <span className="card__footer-value"> {item.ratingsAverage} </span>
-            <span>| </span>
-            <span className="card__footer-text">
-              {" "}
-              {`rating (${item.ratingsQuantity})`}{" "}
-            </span>
-          </p>
-          <a className="btn btn--green btn--small" href={`/tour/${item.slug}`}>
-            {" "}
-            Details{" "}
-          </a>
-        </div>
-      </div>
+class Tours extends Component {
+  state = {
+    tours: [],
+  };
+
+  toursService = new ToursService();
+
+  getTours = () => {
+    this.toursService.getAllTours().then((res) =>
+      this.setState({
+        tours: res.data.data,
+      })
     );
-  });
-  return <>{elements}</>;
-};
+  };
+
+  componentDidMount() {
+    this.getTours();
+  }
+  render() {
+    const { tours } = this.state;
+    const elements = tours.map((item) => {
+      return <Tour key={item._id} item={item} />;
+    });
+    return <div className="card-container">{elements};</div>;
+  }
+}
 
 export default Tours;
