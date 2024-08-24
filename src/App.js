013cad8046b81eec9d942adfba2ed64b9ce318a6
OlegPichkur <oleg1pichkur@gmail.com>
2024-08-24 10:50:19 +0300

    52 + react-transition-group npm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, createRef } from "react";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/layout";
 import LayoutMe from "./components/pages/me/layoutMe/layoutMe";
@@ -7,7 +7,7 @@ import { ForgotPassword, Login, Notfound, Signup } from "./components/pages";
 import Spinner from "./components/spiner/spiner";
 import FormUserSetup from "./components/pages/me/formUserSetup/formUserSetup";
 import MyReviews from "./components/pages/me/myReviews/myReviews";
-import MyTouts from "./components/pages/me/myTours/myTours";
+import MyTours from "./components/pages/me/myTours/myTours";
 import MyBilling from "./components/pages/me/myBilling/myBilling";
 import AdmTours from "./components/pages/me/admTours/admTours";
 import AdmUsers from "./components/pages/me/admUsers/admUsers";
@@ -16,6 +16,57 @@ import AdmBooking from "./components/pages/me/admBooking/admBooking";
 
 const Tours = lazy(() => import("./components/pages/tours/tours"));
 const BibTour = lazy(() => import("./components/pages/bigTour/bigTour"));
+
+const meroutes = [
+  {
+    path: "edit-profile",
+    name: "edit-profile",
+    element: <FormUserSetup />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "my-tours",
+    name: "my-tours",
+    element: <MyTours />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "my-reviews",
+    name: "my-reviews",
+    element: <MyReviews />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "my-billing",
+    name: "my-billing",
+    element: <MyBilling />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "adm-tours",
+    name: "adm-tours",
+    element: <AdmTours />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "adm-users",
+    name: "adm-users",
+    element: <AdmUsers />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "adm-reviews",
+    name: "adm-reviews",
+    element: <AdmReviews />,
+    nodeRef: createRef(),
+  },
+  {
+    path: "adm-booking",
+    name: "adm-booking",
+    element: <adm-booking />,
+    nodeRef: createRef(),
+  },
+];
 
 function App() {
   return (
@@ -29,7 +80,7 @@ function App() {
             <Route path="signup" element={<Signup />} />
             <Route path="me" element={<LayoutMe />}>
               <Route path="edit-profile" element={<FormUserSetup />} />
-              <Route path="my-tours" element={<MyTouts />} />
+              <Route path="my-tours" element={<MyTours />} />
               <Route path="my-reviews" element={<MyReviews />} />
               <Route path="my-billing" element={<MyBilling />} />
               <Route path="adm-tours" element={<AdmTours />} />
